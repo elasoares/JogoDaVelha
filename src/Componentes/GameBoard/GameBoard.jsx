1bd8d2719dfe0bffab9b13d17bde844f3165efe5
Elaine Soares <131168713+elasoares@users.noreply.github.com>
2024-10-04 19:14:41 -0300

    Atualizado

--- src/Componentes/GameBoard/GameBoard.jsx
+++ src/Componentes/GameBoard/GameBoard.jsx
@@ -4,26 +4,10 @@ import styles from "./GameBoard.module.css";
 
 export function GameBoard({onSelectXOrO, board}){
 
-
-
-/* const[pecorrerObjeto, setpercorrendoObjeto] = useState(inicioDoJogo);
-
-function handleClick(rowIndex, colIndex){
-    setpercorrendoObjeto((prev)=>{
-        const percorrer = [...prev.map((percorrendoDentro)=>[...percorrendoDentro])];
-        percorrer[rowIndex][colIndex]=activePlayerSimbol;
-        return percorrer;
-    });
-    onSelectXOrO();
-} */
-
-
-
     return(
         <ol className={`${styles['game-board']} ${styles.listCol}`}>
               {board.map((row, rowIndex)=>(
                     <li key={rowIndex}>
-
                         <ol className={styles.listCol}>
                             {row.map((simboloJogador, colIndex) => (
                                     <li key={colIndex}>
